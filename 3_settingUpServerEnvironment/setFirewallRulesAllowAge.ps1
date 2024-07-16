19a9e597113d9537b86a08e4188b51d60481d8a3
brendanbladdick-adot <BBladdick@esri.com>
2024-07-16 13:41:12 -0600

    fixed setFirewallRulesAllowAge
    The script was not working previously due to the ports array being converted to a string. Keeping the array in the New-NetFirewallRule command works well.

--- 3_settingUpServerEnvironment/setFirewallRulesAllowAge.ps1
+++ 3_settingUpServerEnvironment/setFirewallRulesAllowAge.ps1
@@ -1,20 +1,29 @@
 # This script sets the firewall rules on the specified machines to allow inbound traffic on the specified ports.
 
 # Define the list of machines to set the firewall rules on
-$arcgisservers = @('machine1', 'machine2', 'machine3')
-# Define the list of ports to allow
+$machines = @('machine1', 'machine2', 'machine3')
+
+# Define the ports
 $ports = 80, 135, 443, 445, 2443, 5985, 5986, 6080, 6443, 7080, 7443, 9320, 9220, 9829, 20443, 20301, 21443, 29080, 29081, 29878, 29879, 45672, 45671
 
-$parameters = @{
-  ComputerName = $arcgisservers
-  ScriptBlock = {
-    param($ports)
-    $ports | ForEach-Object {
-      # Create a firewall rule to allow inbound traffic on the specified ports above
-      New-NetFirewallRule -DisplayName "Allow ArcGIS Enterprise Ports In" -Direction Inbound -Action Allow -Protocol TCP -LocalPort $_ -Profile Domain, Private
-    }
-  }
-  ArgumentList = ($ports)
+# Convert the ports array to a comma-separated string
+$portsString = $ports -join ','
+
+# Define the name and description of the rule
+$ruleName = "Allow Inbound ArcGIS Enterprise, Automation and File Share traffic"
+$ruleDescription = "Allows inbound traffic on ports: $portsString"
+
+# Define the script block to be executed on each machine
+$scriptBlock = {
+    param($ports, $ruleName, $ruleDescription)
+
+    # Create the firewall rule for the Domain profile
+    New-NetFirewallRule -DisplayName $ruleName -Description $ruleDescription -Direction Inbound -Action Allow -Protocol TCP -LocalPort $ports -Profile Domain, Private
+
+    Write-Output "Firewall rules created successfully for Domain and Private profiles on $env:COMPUTERNAME."
 }
 
-Invoke-Command @parameters
+# Iterate through each machine and execute the script block
+foreach ($machine in $machines) {
+    Invoke-Command -ComputerName $machine -ScriptBlock $scriptBlock -ArgumentList $ports, $ruleName, $ruleDescription
+}
